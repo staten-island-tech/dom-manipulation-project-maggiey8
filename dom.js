3f2d798b573f47aefb604b722e4f63a4cff37642
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-10-19 10:13:02 -0400

    fgfjrtgrftk

--- dom.js
+++ dom.js
@@ -2,13 +2,23 @@ const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
-    //select the textbox
-    //select all the h2s in one property
 };
-DOMSelectors.form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2s.forEach(
-        (el) => (el.textContent = DOMSelectors.firstName.value)
+
+console.log(DOMSelectors.h2s);
+
+DOMSelectors.form.addEventListener("submit", function(event){
+event.preventDefault();
+
+document
+    .querySelector(".gallery")
+    .insertAdjacentHTML(
+        "afterbegin",
+        `<div class="card"><h2 class="card-header">${DOMSelectors.firstName.value}</h2></div>`
+    )
+
+console.log(DOMSelectors.firstName.value);
+DOMSelectors.h2s.forEach(
+    (el) => (el.textContent = DOMSelectors.firstName.value)
     );
+    
 });
