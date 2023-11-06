082ab2ed7b49761129486cf8dca3c40cee20607c
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-11-06 10:15:33 -0500

    e

--- dom.js
+++ dom.js
@@ -1,19 +1,18 @@
 const DOMSelectors = {
-    form: document.querySelector("#form"),
+    form: document.querySelector(".form"),
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
     link: document.querySelector(".link"),
     imgs: document.querySelectorAll("img")
 };
 
-function makeAlbum() {
-    let input = DOMSelectors.firstName.value
-}
+/* function makeAlbum() {
+    let title = DOMSelectors.firstName.value
+    title.push(DOMSelectors)//???????//
+} */
 
 function addCard() {
-    document
-    .querySelector(".gallery")
-    .insertAdjacentHTML(
+    document.querySelector(".gallery").insertAdjacentHTML(
         "afterend",
     /*     `<div class="card"><h2 class="card-title">${DOMSelectors.firstName.value}</h2></div>` */
             `<div class="gallery" id="box">
@@ -26,26 +25,34 @@ function addCard() {
 
 console.log(DOMSelectors.h2s);
 
-DOMSelectors.form.addEventListener("submit", function(event){
-event.preventDefault();
-    const Album = makeAlbum()
-    addCard(Album)
-    clearFields()
-    addRemoveButtons()
-    
-console.log(DOMSelectors.firstName.value);
-DOMSelectors.h2s.forEach((el) => (el.textContent = DOMSelectors.firstName.value));
+function clearFields() {
+    let inputs = document.querySelectorAll(".form")
+    inputs.clear()
+}
 
-DOMSelectors.img.forEach((el) => (el.textContent) = DOMSelectors.link.value);
-
-});
 function remove() {
     let btn = document.querySelectorAll(".button")
     console.log(btn)
     btn.forEach(button => button.addEventListener("click", function(event) {
     let cardOld = (event.target.parentElement);
     cardOld.remove();
-    }))
+    }
+    ))
 }
 
-remove();
+DOMSelectors.form.addEventListener("submit", function(event){
+event.preventDefault();
+    //const Album = makeAlbum()
+    //addCard(Album)
+    addCard()
+    clearFields()
+    
+console.log(DOMSelectors.firstName.value);
+DOMSelectors.h2s.forEach((el) => (el.textContent = DOMSelectors.firstName.value));
+
+DOMSelectors.img.forEach((el) => (el.textContent) = DOMSelectors.link.value);
+});
+
+DOMSelectors.button.addEventListener("submit", function(event) {
+    remove()
+}) 
